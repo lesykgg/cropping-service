10dba8364109c8eb62e7152f8a3474fbe7af0a95
Oleksii Hashparych <lesykgg@gmail.com>
2017-08-04 21:35:19 +0300

    Removed redundant files, fixed point

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -2,17 +2,33 @@ function getImageCoords(event, img) {
     var rect = img.getBoundingClientRect();
     var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
     var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
-    var color = '#000000';
-    var size = '10px';
-    $("#point")
-        .css('position', 'absolute')
-        .css('top', posY - 5 + 'px')
-        .css('left', posX - 5 + 'px')
-        .css('width', size)
-        .css('height', size)
-        .css('background-color', color)
-        .css('border-radius', '5px');
+
+    movePoint(posX - 5, posY - 5);
+
     var multiplier = $('#hidden_field').text();
     $('#point_x').val(parseInt(posX*multiplier));
     $('#point_y').val(parseInt(posY*multiplier));
 }
+
+function movePoint(x, y) {
+    var color = '#000000';
+    var size = '10px';
+    $("#point")
+        .css('position', 'absolute')
+        .css('top', y + 'px')
+        .css('left', x + 'px')
+        .css('width', size)
+        .css('height', size)
+        .css('background-color', color)
+        .css('border-radius', '5px');
+
+}
+
+$(document).ready(function() {
+    $('#point_x, #point_y').on('input', function() {
+        var multiplier = $('#hidden_field').text();
+        const x = $('#point_x').val() / multiplier;
+        const y = $('#point_y').val() / multiplier;
+        movePoint(x - 5, y - 5);
+    });
+});
